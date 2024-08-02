569a7764711980613053d06066bbece75c5599f3
ricardo-semiao <ricardo.semiao@outlook.com>
2024-08-02 16:26:05 -0300

    Templates: rspkgdown: external site updates (fixed footer to bottom)

--- themes/rspkgdown/dev.R
+++ themes/rspkgdown/dev.R
@@ -6,12 +6,12 @@ for (f in files) {
   reticulate::py_run_string(glue("
   from template_injector import build
   build(
-    'templates/rspkgdown/assets/{f}_template.html',
+    'themes/rspkgdown/assets/{f}_template.html',
     ['site_assets/components.html',
-    'templates/rspkgdown/assets/rspkgdown_components.html'],
-    'templates/rspkgdown/inst/pkgdown/BS5/templates/{f}.html'
+    'themes/rspkgdown/assets/rspkgdown_components.html'],
+    'themes/rspkgdown/inst/pkgdown/BS5/templates/{f}.html'
   )
   "))
 }
 
-devtools::install("templates/rspkgdown")
+devtools::install("themes/rspkgdown")
