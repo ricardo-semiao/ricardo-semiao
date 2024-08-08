1cbc857cf0e56dfebf8da2910ac237d17cf1160b
ricardo-semiao <ricardo.semiao@outlook.com>
2024-08-08 17:20:30 -0300

    Themes: rspkgdown: updated structure

--- themes/rspkgdown/dev.R
+++ themes/rspkgdown/dev.R
@@ -7,7 +7,7 @@ for (f in files) {
   from template_injector import build
   build(
     'themes/rspkgdown/assets/{f}_template.html',
-    ['site_assets/components.html',
+    ['themes/components.html',
     'themes/rspkgdown/assets/rspkgdown_components.html'],
     'themes/rspkgdown/inst/pkgdown/BS5/templates/{f}.html'
   )
