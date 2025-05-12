61c96be41de23705897c0a3c826e0efa0c30edb5
ricardo-semiao <ricardo.semiao@outlook.com>
2025-05-12 15:21:06 -0300

    Themes: adjusted rspkgdown naming convention

--- themes/rspkgdown/dev.R
+++ themes/rspkgdown/dev.R
@@ -6,12 +6,18 @@ for (f in files) {
   reticulate::py_run_string(glue("
   from template_injector import build
   build(
-    'themes/rspkgdown/template_build/{f}_template.html',
-    ['themes/components.html',
-    'themes/rspkgdown/template_build/rspkgdown_components.html'],
+    'themes/rspkgdown/assets/{f}_template.html',
+    ['themes/site_components.html',
+    'themes/rspkgdown/assets/rspg_components.html'],
     'themes/rspkgdown/inst/pkgdown/BS5/templates/{f}.html'
   )
   "))
 }
+
+file.copy(
+  "themes/rspkgdown/assets/rspg_style.css",
+  "themes/rspkgdown/inst/pkgdown/BS5/assets/rspg_style.css",
+  overwrite = TRUE
+)
 
 devtools::install("themes/rspkgdown")
