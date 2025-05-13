a43e19d42015249f2e114baf517d4934c45e1b91
ricardo-semiao <ricardo.semiao@outlook.com>
2025-05-13 00:30:24 -0300

    Updated rspkgdown template following pkgdown commit c368449
    - Updated to the new folder names
    - Added post_render.R scripts to correct some bad interactions between pkgdown and rspkgdown
    - Removed unused templates
    - Pretty indented templates

--- themes/rspkgdown/dev.R
+++ themes/rspkgdown/dev.R
@@ -2,7 +2,7 @@ library(devtools)
 library(glue)
 
 # Building templates
-files <- c("head", "layout") #, "navbar"
+files <- c("head", "layout")
 
 for (f in files) {
   reticulate::py_run_string(glue("
@@ -11,7 +11,7 @@ for (f in files) {
     'themes/rspkgdown/assets/{f}_template.html',
     ['themes/site_components.html',
     'themes/rspkgdown/assets/rspg_components.html'],
-    'themes/rspkgdown/inst/pkgdown/BS5/templates/{f}.html'
+    'themes/rspkgdown/inst/pkgdown/templates/{f}.html'
   )
   "))
 }
@@ -20,9 +20,12 @@ for (f in files) {
 # Copying assets to package folders
 file.copy(
   "themes/rspkgdown/assets/rspg_style.css",
-  "themes/rspkgdown/inst/pkgdown/BS5/assets/rspg_style.css",
+  "themes/rspkgdown/inst/pkgdown/assets/rspg_style.css",
   overwrite = TRUE
 )
 
 # Building package
-install("themes/rspkgdown")
+load_all("themes/rspkgdown")
+document("themes/rspkgdown")
+check("themes/rspkgdown")
+install("themes/rspkgdown", upgrade = FALSE)
